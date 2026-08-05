cdcfd472b807b69eec8ea5768b1b0d72334bc63e
john-cpu25 <nhan.nguyenthanh0894@gmail.com>
2026-08-05 12:20:14 +0700

    Fix ID generation to use UUID for Supabase

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@
 const App = (() => {
   // ---- Utilities ----
   function generateId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2, 9);
+    if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+      return crypto.randomUUID();
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
   function formatDate(isoString) {
